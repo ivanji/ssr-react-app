14ab22e1daf2bc88e18c8b46dd7916ea429bff27
Ivan J <ivanji@gmail.com>
2017-10-27 16:24:02 +1100

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import getFacts from './facts';
+
+getFacts().then(facts => {
+   ReactDOM.render(<App facts={facts()}>,document.getElementById('app'));
+});
+
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    entry: {
+
+    },
+    module: {
+
+    },
+    output: {
+
+    }
+};
